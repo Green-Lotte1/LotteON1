c9806a26a6b503c1e01adfd1ae34bce2b4634e0b
dlwlals6790 <dlwlals6790@gmail.com>
2023-10-13 17:36:31 +0900

    css 설정

--- src/main/java/co/kr/lotteon/controller/member/MemberController.java
+++ src/main/java/co/kr/lotteon/controller/member/MemberController.java
@@ -1,4 +1,29 @@
 package co.kr.lotteon.controller.member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
+
+    @GetMapping("/member/join")
+    public String join(){
+        return "/member/join";
+    }
+    @GetMapping("/member/login")
+    public String login(){
+        return "/member/login";
+    }
+    @GetMapping("/member/register")
+    public String register(){
+        return "/member/register";
+    }
+    @GetMapping("/member/registerSeller")
+    public String registerSeller(){
+        return "/member/registerSeller";
+    }
+    @GetMapping("/member/signup")
+    public String signup(){
+        return "/member/signup";
+    }
 }
