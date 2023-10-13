d5bffc521571a348ac5bff2e349c37a3fb695394
dlwlals6790 <dlwlals6790@gmail.com>
2023-10-13 15:58:44 +0900

    프로젝트

---
+++ src/main/java/co/kr/lotteon/controller/MainController.java
@@ -0,0 +1,13 @@
+package co.kr.lotteon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/","/index"})
+    public String index(){
+        return "/index";
+    }
+}
