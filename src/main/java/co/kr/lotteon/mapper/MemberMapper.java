bb58d80590050453ea79a2ba1847dbe8c7b1b00b
dlwlals6790 <dlwlals6790@gmail.com>
2023-10-16 21:33:27 +0900

    일부수정

--- src/main/java/co/kr/lotteon/mapper/MemberMapper.java
+++ src/main/java/co/kr/lotteon/mapper/MemberMapper.java
@@ -2,10 +2,12 @@ package co.kr.lotteon.mapper;
 
 
 import co.kr.lotteon.dto.MemberDTO;
+import co.kr.lotteon.dto.member.TermsDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
     public void insertMember(MemberDTO dto);
+    public TermsDTO selectTerms();
 
 }
