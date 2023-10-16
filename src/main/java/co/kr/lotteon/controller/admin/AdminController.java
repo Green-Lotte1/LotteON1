35c569fe6d1368d6a770c86bf46f52ddf076dfb7
tngus78901 <tngus78901@gmail.com>
2023-10-16 12:33:10 +0900

    admin index,product register html 경로설정

--- src/main/java/co/kr/lotteon/controller/admin/AdminController.java
+++ src/main/java/co/kr/lotteon/controller/admin/AdminController.java
@@ -1,6 +1,47 @@
 package co.kr.lotteon.controller.admin;
 
+import co.kr.lotteon.service.AdminService;
+import co.kr.lotteon.service.CsService;
+import co.kr.lotteon.service.MemberService;
+import co.kr.lotteon.service.ProductService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private CsService csService;
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping(value = "/product/view")
+    public String view() {
+        return "/product/view";
+    }
+    @GetMapping(value = "/product/cart")
+    public String cart() {
+        return "/product/cart";
+    }
+    @GetMapping(value = "/product/order")
+    public String order() {
+        return "/product/order";
+    }
+    @GetMapping(value = "/product/complete")
+    public String complete() {
+        return "/product/complete";
+    }
+
 
 
 }
+
+
+
--- src/main/java/co/kr/lotteon/service/AdminService.java
+++ src/main/java/co/kr/lotteon/service/AdminService.java
@@ -1,4 +1,29 @@
 package co.kr.lotteon.service;
 
+import co.kr.lotteon.repository.MemberRepository;
+import co.kr.lotteon.repository.ProductRepository;
+import co.kr.lotteon.repository.cs.CsRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+
+@Log4j2
+@Builder
+@RequiredArgsConstructor
+@Service
 public class AdminService {
+
+    private final ModelMapper modelMapper;
+    private final CsRepository csRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+
+
+
+
+
+
 }
