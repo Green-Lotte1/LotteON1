692bb7e32ee699f9981cae6fc18c516c3a6bacd7
dlwlals6790 <dlwlals6790@gmail.com>
2023-10-16 17:35:35 +0900

    mybatis 추가

--- src/main/java/co/kr/lotteon/service/MemberService.java
+++ src/main/java/co/kr/lotteon/service/MemberService.java
@@ -2,6 +2,7 @@ package co.kr.lotteon.service;
 
 import co.kr.lotteon.dto.MemberDTO;
 import co.kr.lotteon.entity.MemberEntity;
+import co.kr.lotteon.mapper.MemberMapper;
 import co.kr.lotteon.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,18 +12,17 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void save(MemberDTO dto){
+    @Autowired
+    private MemberMapper memberMapper;
+
+    public void insert(MemberDTO dto){
         dto.setKm_pass1(passwordEncoder.encode(dto.getKm_pass1()));
         // DTO를 Entity로 변환
         MemberEntity entity = dto.toEntity();
 
-        // DB insert
-        memberRepository.save(entity);
+        memberMapper.insertMember(dto);
     }
 
 }
