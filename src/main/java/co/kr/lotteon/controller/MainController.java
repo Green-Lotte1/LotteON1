1f9e78ef40c1ec5da77e5429ee5fa4c10e9ef9d5
lomong7807 <lomong7807@gmail.com>
2023-10-19 09:30:03 +0900

    appVersion

--- src/main/java/co/kr/lotteon/controller/MainController.java
+++ src/main/java/co/kr/lotteon/controller/MainController.java
@@ -2,9 +2,11 @@ package co.kr.lotteon.controller;
 
 import co.kr.lotteon.dto.product.ProdCate1DTO;
 import co.kr.lotteon.dto.product.ProdCate2DTO;
+import co.kr.lotteon.service.MainService;
 import co.kr.lotteon.service.ProductService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +19,17 @@ public class MainController {
 
     @Autowired
     private ProductService prodService;
+    @Autowired
+    private MainService mainService;
 
     @GetMapping(value = {"/","/index"})
     public String index(Model model){
         List<ProdCate1DTO> cate1 = prodService.selectAllProdCate1();
         List<ProdCate2DTO> cate2 = prodService.selectAllProdCate1AndProdCate2();
-
         model.addAttribute("cate1List", cate1);
         model.addAttribute("cate2List", cate2);
 
+        mainService.appVersion(model);
 
         return "/index";
     }
