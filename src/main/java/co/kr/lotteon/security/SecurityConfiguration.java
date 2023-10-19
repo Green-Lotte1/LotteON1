327e370d45388560983097fa6b1b8bb7c8390050
dlwlals6790 <dlwlals6790@gmail.com>
2023-10-19 12:44:22 +0900

    member

--- src/main/java/co/kr/lotteon/security/SecurityConfiguration.java
+++ src/main/java/co/kr/lotteon/security/SecurityConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfiguration {
